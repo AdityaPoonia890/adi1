84aedc4e3f6f6b79a31e597cb90182b5fa0dabc2
aditya <aspoonia2018@gmail.com>
2024-09-22 21:56:01 +0530

    second commit

--- src/main/java/com/aditya/ecom_project/model/Product.java
+++ src/main/java/com/aditya/ecom_project/model/Product.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Component;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
 import lombok.AllArgsConstructor;
@@ -19,14 +21,15 @@ import lombok.NoArgsConstructor;
 @Component
 public class Product {
 
- @Id   
+ @Id  
+ @GeneratedValue(strategy = GenerationType.IDENTITY) 
  private int id;
  private String name;
  private String description;
  private String brand;
  private int price;
  private String category;
- @JsonFormat(shape=JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy")
+ 
  private Date releaseDate;
  private boolean productAvailable;
  private int stockQuantity;
